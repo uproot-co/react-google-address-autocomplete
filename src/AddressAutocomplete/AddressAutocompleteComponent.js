85108532eebc0c14cfc27e6805805d3eaffc01b6
SAnschutz <23403170+SAnschutz@users.noreply.github.com>
2021-06-27 12:41:44 -0500

    adding css modules

--- src/AddressAutocomplete/AddressAutocompleteComponent.js
+++ src/AddressAutocomplete/AddressAutocompleteComponent.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Input from './Input'
 import AddressDropdown from './AddressDropdown'
 import SubmitButton from './SubmitButton'
+import styles from './AddressAutocomplete.module.css'
 
 const ReactGoogleAddressAutocomplete = ({
   boundsReference,
@@ -12,6 +13,9 @@ const ReactGoogleAddressAutocomplete = ({
   userOnInputChange, // optional --   any additional functionality that an input change needs to trigger
   inputPlaceholder,
   inputAutoFocus = true,
+  toggleIcon,
+  userOnToggle,
+  useDefaultToggleIcon = false,
   customSubmitButton,
   displayDefaultSubmitButton,
   defaultSubmitButtonIsDisabled,
@@ -28,19 +32,20 @@ const ReactGoogleAddressAutocomplete = ({
   const [predictions, setPredictions] = useState([])
   const [inputValue, setInputValue] = useState('')
   const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+  const [clearX, setClearX] = useState(false)
 
   useEffect(() => {
     predictions.length > 1 ? setIsDropdownOpen(true) : setIsDropdownOpen(false)
   }, [predictions])
 
   useEffect(() => {
+    setClearX(inputValue.length >= 1)
     if (inputValue) {
       const getPredictions = async () => {
         const predictions = await fetchPredictions(inputValue)
         predictions && setPredictions(predictions)
       }
       !selectedAddress && getPredictions()
-      console.log(inputValue) // REMOVE LATER
     }
   }, [inputValue])
 
@@ -49,7 +54,9 @@ const ReactGoogleAddressAutocomplete = ({
   }, [error])
 
   useEffect(() => {
-    selectedAddress.length && onAddressSelected && onAddressSelected(selectedAddress)
+    selectedAddress.length &&
+      onAddressSelected &&
+      onAddressSelected(selectedAddress)
   }, [selectedAddress])
 
   const handleOnChange = (event) => {
@@ -66,51 +73,94 @@ const ReactGoogleAddressAutocomplete = ({
     setIsDropdownOpen(false)
   }
 
+  const handleToggle = () => {
+    setSelectedAddress('')
+    setInputValue('')
+    userOnToggle && userOnToggle()
+  }
+
   const handleOnSubmit = () => {
     inputValue
       ? onClickSubmitButton(inputValue)
       : setInputAddressError('Please enter an address')
   }
 
+  const RGAAInputProps = {
+    [onChangeName]: handleOnChange,
+    value: inputAddressError || selectedAddress || inputValue,
+    onFocus: () => setClearX(inputValue.length >= 1),
+    onBlur: () => setClearX(false)
+  }
+  console.log(styles.input, ' input')
+
   return (
     <div>
-      {CustomInput ? (
-        <CustomInput
-          {...customInputProps}
-          {...{
-            [onChangeName]: handleOnChange,
-            value: inputAddressError || selectedAddress || inputValue
-          }}
-        />
-      ) : (
-        <Input
-          placeholder={inputPlaceholder}
-          value={inputAddressError || selectedAddress || inputValue}
-          autoFocus={inputAutoFocus}
-          onChange={handleOnChange}
-          error={inputAddressError}
-          userDefinedStyles={inputStyles}
-        />
-      )}
-      {customSubmitButton && customSubmitButton}
-      {displayDefaultSubmitButton && (
-        <SubmitButton
-          isDisabled={defaultSubmitButtonIsDisabled}
-          userDefinedStyles={submitButtonStyles}
-          onClick={handleOnSubmit}
-        />
-      )}
-      {isDropdownOpen ? (
-        <AddressDropdown
-          predictions={predictions}
-          boundsReference={boundsReference}
-          onSelect={handleAddressSelected}
-          onClickOutside={() => setIsDropdownOpen(false)}
-          pinIcon={pinIcon}
-          setIsDropdownOpen={setIsDropdownOpen}
-          userDefinedStyles={addressDropdownStyles}
-        />
-      ) : null}
+      <div className={styles.hi}>This should be in hot pink</div>
+      <div className={styles.inputContainer}>
+        <span
+          className={styles.inputFieldWrapper}
+          style={toggleIcon && { display: 'flex' }}
+        >
+          {CustomInput ? (
+            <CustomInput
+              style={toggleIcon && { width: '87.6%' }}
+              {...customInputProps}
+              {...RGAAInputProps}
+            />
+          ) : (
+            <Input
+              {...RGAAInputProps}
+              placeholder={inputPlaceholder}
+              autoFocus={inputAutoFocus}
+              error={inputAddressError}
+              userDefinedStyles={inputStyles}
+              // className={
+              //   (styles.input,
+              //   defaultSubmitButtonIsDisabled && styles.disabled,
+              //   toggleIcon && styles.withIcon)
+              // }
+            />
+          )}
+          {toggleIcon && clearX && (
+            <button
+              onClick={handleToggle}
+              style={{ display: 'inline-flex' }}
+              type='button'
+              className={styles.toggleButton}
+            >
+              {toggleIcon}
+            </button>
+          )}
+          {useDefaultToggleIcon && (
+            <button
+              onClick={handleToggle}
+              className={styles.iconBtn}
+              type='button'
+            >
+              X
+            </button>
+          )}
+        </span>
+        {customSubmitButton && customSubmitButton}
+        {displayDefaultSubmitButton && (
+          <SubmitButton
+            isDisabled={defaultSubmitButtonIsDisabled}
+            userDefinedStyles={submitButtonStyles}
+            onClick={handleOnSubmit}
+          />
+        )}
+        {isDropdownOpen ? (
+          <AddressDropdown
+            predictions={predictions}
+            boundsReference={boundsReference}
+            onSelect={handleAddressSelected}
+            onClickOutside={() => setIsDropdownOpen(false)}
+            pinIcon={pinIcon}
+            setIsDropdownOpen={setIsDropdownOpen}
+            userDefinedStyles={addressDropdownStyles}
+          />
+        ) : null}
+      </div>
     </div>
   )
 }
